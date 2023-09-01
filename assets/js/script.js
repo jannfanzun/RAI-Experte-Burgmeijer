f88b9dc681586f2d664e37605039fc6a49636c36
jannfanzun <110892321+jannfanzun@users.noreply.github.com>
2023-09-01 22:52:20 +0200

    wip: arrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -201,3 +201,36 @@ if($('#map').length >0 ) {
   }
   google.maps.event.addDomListener(window, 'load', init);
 }
+
+const toggleButton = document.getElementById('toggleButton');
+        const sectionToToggle = document.querySelector('.preisesection'); // Ändere dies zu der gewünschten Section
+
+        toggleButton.addEventListener('click', () => {
+            if (sectionToToggle.classList.contains('hidden')) {
+                sectionToToggle.classList.remove('hidden');
+                toggleButton.textContent = 'Preisansicht schliessen';
+            } else {
+                sectionToToggle.classList.add('hidden');
+                toggleButton.textContent = 'Preisansicht öffnen';
+            }
+        });
+
+        // Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
